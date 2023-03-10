a142f3f5266d61eab31dabd5834fc83aeb7a5e29
silviosalza <silviofilipposalza@gmail.com>
2023-03-10 16:52:12 +0100

    css implemented

--- js/script.js
+++ js/script.js
@@ -14,22 +14,28 @@
 //  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
 
-
+const gridElem = document.querySelector(".grid");
 for (let i = 1; i <=100; i++){
 
     let result;
+    let classColor
     if ((i % 3 === 0) && (i % 5 === 0)) {
         result = "FizzBuzz";
+        classColor = "FBcolor"
     } else if (i % 5 === 0){
         result = "Buzz"
+        classColor = "Bcolor"
     } else if (i % 3 === 0){
         result= "Fizz"
+        classColor = "Fcolor"
     } else{
         result = i
+        classColor = "main-color"
     }
 
     console.log(result);
+    gridElem.innerHTML += `<div class="box ${classColor}">${result}</div>`;
 
-    
+
 
 }
