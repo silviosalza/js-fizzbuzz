0b1230ce84514bcbe8f0daeaf3910b716aca61fc
silviosalza <silviofilipposalza@gmail.com>
2023-03-13 10:29:46 +0100

    create elem example

--- js/script.js
+++ js/script.js
@@ -18,24 +18,51 @@ const gridElem = document.querySelector(".grid");
 for (let i = 1; i <=100; i++){
 
     let result;
-    let classColor
+    let classColor;
     if ((i % 3 === 0) && (i % 5 === 0)) {
         result = "FizzBuzz";
-        classColor = "FBcolor"
+        classColor = "FBcolor";
     } else if (i % 5 === 0){
-        result = "Buzz"
-        classColor = "Bcolor"
+        result = "Buzz";
+        classColor = "Bcolor";
     } else if (i % 3 === 0){
-        result= "Fizz"
-        classColor = "Fcolor"
+        result= "Fizz";
+        classColor = "Fcolor";
     } else{
-        result = i
-        classColor = "main-color"
+        result = i;
+        classColor = "main-color";
     }
 
     console.log(result);
     gridElem.innerHTML += `<div class="box ${classColor}">${result}</div>`;
 
-
-
 }
+
+//metodo alternativo, utile per creare event listener alla creazione
+
+// const gridElem = document.querySelector(".grid");
+// for (let i = 1; i <=100; i++){
+
+//     let result;
+//     let classColor;
+//     if ((i % 3 === 0) && (i % 5 === 0)) {
+//         result = "FizzBuzz";
+//         classColor = "FBcolor";
+//     } else if (i % 5 === 0){
+//         result = "Buzz";
+//         classColor = "Bcolor";
+//     } else if (i % 3 === 0){
+//         result= "Fizz";
+//         classColor = "Fcolor";
+//     } else{
+//         result = i;
+//         classColor = "main-color";
+//     }
+
+//     console.log(result);
+//     const newBox = document.createElement("div");
+//     newBox.innerHTML = result ;
+//     newBox.classList.add("box");
+//     newBox.classList.add(classColor)
+
+// }
